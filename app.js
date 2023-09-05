21f21532721fd8ee29394c44885d123a9012a4f3
mohamed asdaf <krastiy13@gmail.com>
2023-09-05 19:04:49 +0200

    primer comit

---
+++ app.js
@@ -0,0 +1,68 @@
+import express from "express";
+import {
+    getTodo,
+    getSharedTodoByID,
+    getUserById,
+    getUserByEmail,
+    createTodo,
+    deletteTodo,
+    toggleCompleted,
+    shareTodo,
+    getTodosById
+} from "./database.js"
+import cors from "cors"
+
+// const corsOption = {
+//     origin: "exp://192.168.1.160:8081",
+//     methods: ["POST", "GET"],
+//     credential: true
+// }
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/todos/:id", async (req, res) => {
+    const todos = await getTodosById(req.params.id)
+    res.status(200).send(todos)
+})
+app.get("/todos/shared_todos/:id", async (req, res) => {
+
+    const todoID = await getSharedTodoByID(req.params.id);
+    console.log(todoID, "node node node node node node ")
+    const author = await getUserById(todoID.user_id);
+    const shared_with = await getUserById(todoID.shared_with_id);
+    res.status(200).send({ author, shared_with })
+
+})
+app.get("/users/:id", async (req, res) => {
+    const user = await getUserById(req.params.id);
+    res.status(200).send(user)
+})
+
+app.put("/todos/:id", async (req, res) => {
+    const { value } = req.body
+
+    const todo = await toggleCompleted(req.params.id, value)
+    res.status(200).send(todo);
+})
+app.delete("/todos/:id", async (req, res) => {
+    await deletteTodo(req.params.id);
+    res.status(200).send({ message: "Todo deleted successfully" });
+})
+
+app.post("/todos/shared_todos", async (req, res) => {
+    const { todo_id, user_id, email } = req.body
+    const userToShare = await getUserByEmail(email);
+    const sharedTodo = await shareTodo(todo_id, user_id, userToShare.id);
+    res.status(201).send(sharedTodo);
+})
+app.post("/todos", async (req, res) => {
+    const { user_id, title } = req.body;
+    const todo = await createTodo(user_id, title);
+    res.status(201).send(todo);
+})
+const puerto = 3000
+app.listen(puerto, () => {
+    console.log(`server running in port ${puerto}`)
+})
---
+++ database.js
@@ -0,0 +1,108 @@
+import mysql from "mysql2";
+import dotenv from "dotenv"
+dotenv.config()
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+})
+    .promise();
+
+export const getTodosById = async (id) => {
+
+    const [rows] = await pool.query(
+        `SELECT todos.* , shared_todos.shared_with_id
+        FROM todos
+        LEFT JOIN shared_todos ON todos.id = shared_todos.todo_id
+        WHERE todos.user_id = ? OR shared_todos.shared_with_id = ?`,
+        [id, id]);
+    return rows;
+}
+
+
+export const getTodo = async (id) => {
+    const [rows] = await pool.query(
+        `SELECT * FROM  todos WHERE id = ? `, [id]
+
+    );
+    return rows[0]
+}
+
+export const getSharedTodoByID = async (id) => {
+
+    const [rows] = await pool.query(
+        `SELECT * FROM shared_todos WHERE todo_id = ?`,
+        [id]
+    )
+
+    return rows[0]
+}
+export const getUserById = async (id) => {
+    const [rows] = await pool.query(`SELECT * FROM users WHERE id = ?`, [id]);
+    return rows[0]
+}
+
+
+export const getUserByEmail = async (email) => {
+    const [rows] = await pool.query(`SELECT * FROM users WHERE email = ?`, [email]);
+
+    return rows[0]
+}
+
+export const createTodo = async (user_id, title) => {
+
+    const [result] = await pool.query(`INSERT INTO todos (user_id,title)
+    VALUES (?,?)`, [user_id, title]
+    );
+    const todoID = result.insertId;
+    return getTodo(todoID)
+
+}
+
+
+export const deletteTodo = async (id) => {
+    const [results] = await pool.query(
+        `
+        DELETE FROM todos WHERE id = ?`,
+        [id]
+    );
+    return results
+
+}
+
+export const toggleCompleted = async (id, value) => {
+
+    const newValue = value === true ? "TRUE" : "FALSE";
+
+    const [results] = await pool.query(
+        `
+        UPDATE todos
+        SET completed = ${newValue}
+        WHERE id = ?
+        `, [id]
+    );
+    return results;
+
+}
+
+export const shareTodo = async (todo_id, user_id, shared_with_id) => {
+
+    const results = await pool.query(`
+    INSERT INTO shared_todos (todo_id,user_id,shared_with_id)
+    VALUES (?,?,?)
+    `, [todo_id, user_id, shared_with_id]);
+
+    return results.insertId
+
+
+}
+
+
+
+
+
+
+
+
